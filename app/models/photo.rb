8974cf71df875c78093652c2692154dcb97a94fc
osha7 <ogroetz@gmail.com>
2023-01-17 03:44:47 +0000

    scope methods on photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,4 +7,10 @@ class Photo < ApplicationRecord
 
   validates :caption, presence: true
   validates :image, presence: true
+
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+  scope :by_likes, -> { order(likes_count: :desc) }
+  # allows us to:
+  # current_user.discover.past_week.by_likes
+  
 end

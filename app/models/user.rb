28f71a399481755dcb2c9faba8df2abda1f39497
osha7 <ogroetz@gmail.com>
2023-01-17 03:26:28 +0000

    likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: "author_id"
   has_many :follow_requests, foreign_key: "recipient_id"
   has_many :follow_requests, foreign_key: "sender_id"
+  has_many :likes, foreign_key: "fan_id"
 end

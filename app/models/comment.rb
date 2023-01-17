82d57bf37f57e9c5230351594352c4467f23938b
osha7 <ogroetz@gmail.com>
2023-01-17 03:40:09 +0000

    counter cache

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: "User"
-  belongs_to :photo
+  belongs_to :author, class_name: "User", counter_cache: true
+  belongs_to :photo, counter_cache: true
   validates :body, presence: true
 end

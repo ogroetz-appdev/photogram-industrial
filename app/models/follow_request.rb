1726cc5e9b20468bab256d4f18da0f5f0ba62e46
osha7 <ogroetz@gmail.com>
2023-01-17 03:59:17 +0000

    follow requests status update

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -1,4 +1,17 @@
 class FollowRequest < ApplicationRecord
   belongs_to :recipient, class_name: "User"
   belongs_to :sender, class_name: "User"
+
+  validates :recipient_id, uniqueness: { scope: :sender_id, message: "already followed" }
+
+  # ActiveRecord::Enum, which will give us a bunch of handy querying and other methods for free:
+  # for 'status' field of table:
+  enum status: { pending: "pending", rejected: "rejected", accepted: "accepted" }
+#   # assume follow_request is a valid and pending
+# follow_request.accepted? # => false
+# follow_request.accepted! # sets status to "accepted" and saves
+# We also get automatic positive and negative scopes:
+
+# FollowReqest.accepted
+# current_user.received_follow_requests.not_rejected
 end
---
+++ db/migrate/20230117035605_alter.rb
@@ -0,0 +1,9 @@
+class Alter < ActiveRecord::Migration[6.1]
+  def change
+    change_column_default(
+      :follow_requests,
+      :status,
+      "pending",
+    )
+  end
+end
